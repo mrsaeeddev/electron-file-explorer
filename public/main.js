cfb22a08ba7c469cd682e25a3130ac7e34c6075c
mrsaeeddev <saeed_dev@yahoo.com>
2021-12-18 15:20:40 +0500

    Setup electron and add other necessary packages

---
+++ public/main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron')
+
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadURL("http://localhost:3000")
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+    if (process.platform != 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
